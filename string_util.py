6f14370daf5f710f934c3ee2549016969c71ffbb
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2020-09-03 19:42:57 -0400

    Handle almost equals of None strings

--- string_util.py
+++ string_util.py
@@ -9,6 +9,9 @@ def capture(input: str, regex: str, pattern_flags: int = 0, groupnum: int = 1) -
 
 
 def almost_equal(str1: str, str2: str) -> bool:
-    str1_processed = re.sub(r'\W+', '', str1.strip().casefold())
-    str2_processed = re.sub(r'\W+', '', str2.strip().casefold())
-    return str1_processed == str2_processed
+    if str1 is None or str2 is None:
+        return str1 is None and str2 is None
+    else:
+        str1_processed = re.sub(r'\W+', '', str1.strip().casefold())
+        str2_processed = re.sub(r'\W+', '', str2.strip().casefold())
+        return str1_processed == str2_processed
--- tests/test_string_util.py
+++ tests/test_string_util.py
@@ -5,6 +5,10 @@ from string_util import *
 
 class Test(TestCase):
     def test_almost_equal(self):
+        self.assertTrue(almost_equal(None, None))
+        self.assertFalse(almost_equal('', None))
+        self.assertFalse(almost_equal(None, ''))
+
         self.assertTrue(almost_equal('abc', 'abc'))
         self.assertTrue(almost_equal('abc', 'ABC'))
         self.assertTrue(almost_equal('abc', 'abc '))
