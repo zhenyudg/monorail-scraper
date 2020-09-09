df141dd5d94d4102f53fddc29e4c4d48d4695cac
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2020-09-08 22:01:53 -0400

    stress test

---
+++ tests/stress_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+from issue.issue_scraper import IssueScraper, get_issue_url
+from oss_fuzz.oss_fuzz_bug_report_parser import attach_oss_fuzz_bug_report
+
+
+class StressTest(unittest.TestCase):
+
+    def test_stress(self):
+        scraper = IssueScraper()
+        url = get_issue_url('oss-fuzz', 24513)
+
+        iterations = 100
+        fails = 0
+
+        for i in range(iterations):
+            try:
+                issue = scraper.scrape(url)
+                attach_oss_fuzz_bug_report(issue)
+            except Exception as e:
+                print(f"Iteration {i} failed:")
+                print(e)
+                fails += 1
+
+        print(f"{fails} failed out of {iterations} iterations.")
